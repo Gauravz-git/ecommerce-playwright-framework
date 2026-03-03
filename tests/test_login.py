92089f47ae7fb90c3577e8b237dea9f358656298
Gauravz-git <gauravc0223@gmail.com>
2026-03-03 14:08:52 +0530

    pytest

---
+++ tests/test_login.py
@@ -0,0 +1,9 @@
+from pages.login_page import LoginPage
+
+def test_successful_login(page):
+    page.goto("https://www.saucedemo.com/")
+    login = LoginPage(page)
+
+    login.login("standard_user", "secret_sauce")
+
+    assert "inventory" in page.url
