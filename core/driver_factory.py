9f5b6ccc99ffe401f8d57035fb6bee8f007b3bf1
Gauravz-git <gauravc0223@gmail.com>
2026-03-03 14:55:16 +0530

    added slomo

--- core/driver_factory.py
+++ core/driver_factory.py
@@ -1,14 +1,36 @@
 from playwright.sync_api import sync_playwright
 
+
 class DriverFactory:
-    def __init__(self, browser="chromium", headless=False):
-        self.playwright = None
+    def __init__(self, browser="chromium", headless=False, slow_mo=0):
         self.browser_name = browser
         self.headless = headless
+        self.slow_mo = slow_mo
+        self.playwright = None
+        self.browser = None
+        self.context = None
+        self.page = None
 
     def launch_browser(self):
         self.playwright = sync_playwright().start()
-        browser = getattr(self.playwright, self.browser_name).launch(headless=self.headless)
-        context = browser.new_context()
-        page = context.new_page()
-        return page, browser, self.playwright
+
+        browser_type = getattr(self.playwright, self.browser_name)
+
+        self.browser = browser_type.launch(
+            headless=self.headless,
+            slow_mo=self.slow_mo
+        )
+
+        self.context = self.browser.new_context(
+            record_video_dir="reports/videos/"
+        )
+
+        self.page = self.context.new_page()
+
+        return self.page
+
+    def quit_browser(self):
+        if self.browser:
+            self.browser.close()
+        if self.playwright:
+            self.playwright.stop()
